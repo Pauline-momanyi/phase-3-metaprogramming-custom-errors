08924e93805d022160b09edf926af8f62afcaaf7
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-06-10 15:46:21 +0300

    commit tree c1fa667f5de2ade09e10f57d5d75f6590b0873c1

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,7 @@
+# NoMethodError
+# ArgumentError
+# SyntaxError
+require 'pry'
 class Person
   attr_accessor :partner, :name
 
@@ -6,15 +10,39 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
+  end
+
+  def itself
+    self 
+  end
+  # binding.pry
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  
   end
 
 end
 
 beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
+puts beyonce.get_married('jay')
+puts
+# puts beyonce.get_married
 puts beyonce.name
+puts beyonce.itself
+puts beyonce
+puts Person
 
 
 
